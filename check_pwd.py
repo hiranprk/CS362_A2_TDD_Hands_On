67fa7728b089de84c1837cfe35bc4e1b345de5be
Kongkom Hiranpradit <hiranprk@oregonstate.edu>
2023-11-12 13:16:37 +0700

    Add test 1

--- check_pwd.py
+++ check_pwd.py
@@ -2,4 +2,3 @@
 # A2: TDD Hands On
 # by Kongkom Hiranpradit ONID ID: hiranprk
 # 12 November 2023
-
--- tests.py
+++ tests.py
@@ -3,3 +3,19 @@
 # by Kongkom Hiranpradit ONID ID: hiranprk
 # 12 November 2023
 
+import unittest
+from check_pwd import check_pwd
+
+
+class TestCase(unittest.TestCase):
+
+    """ Rule 1: Must be between 8 and 20 characters (inclusive) """
+    def test1(self):
+        """ Boundary case: 7 characters """
+        input = 'aldjfldi'
+        expected = False
+        self.assertEqual(check_pwd(input), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
